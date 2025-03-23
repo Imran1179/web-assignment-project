f5353768df95fd6ec14881888e401e9056388c65
Imran1179 <imran0152177@gmail.com>
2025-03-23 20:52:56 +0600

    fourth commit done

--- script/downloadCv.js
+++ script/downloadCv.js
@@ -1,7 +1,7 @@
 document.getElementById("downloadCV").addEventListener("click", function () {
     const link = document.createElement("a");
-    link.href = ""; // সঠিক URL দিন
-    link.setAttribute("download", "Imran_CV.pdf"); // ফাইল নাম সেট করুন
+    link.href = ""; 
+    link.setAttribute("download", "Imran_CV.pdf"); 
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
