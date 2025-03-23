4b50a2a2d44eb313819c8274bccc98bfa3325774
Imran1179 <imran0152177@gmail.com>
2025-03-23 19:33:07 +0600

    first commit done

---
+++ script/script.js
@@ -0,0 +1,11 @@
+document.getElementById("contactBtn").addEventListener("click", function () {
+    document.getElementById("contactModal").style.display = "flex";
+});
+
+document.getElementById("closeModal").addEventListener("click", function () {
+    document.getElementById("contactModal").style.display = "none";
+});
+
+document.getElementById("closeModalBtn").addEventListener("click", function () {
+    document.getElementById("contactModal").style.display = "none";
+});
